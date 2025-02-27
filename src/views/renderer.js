89fd07e56c52231f59127800a5b57658d77ca97f
ryananimo64 <ryananimo64@gmail.com>
2025-02-27 13:44:09 -0300

    .

---
+++ src/views/renderer.js
@@ -0,0 +1,17 @@
+/**
+ * processo de renderização
+ * tela principal
+ */
+
+console.log("Processo de renderização")
+
+function cliente() {
+    //console.log('VAI CORITHIANS')
+    //uso da api no
+    api.clientwindow()
+}
+
+function os() {
+    //console.log('VAI CORITHIANS')
+    api.oswindow()
+}
